11c054ed12fa7c39c079e46e68c2fca9f0b896ae
TJVasquezSchool <tvasquez@cpp.edu>
2026-03-20 22:21:42 -0700

    Added use case methods

---
+++ Ice Cream Ordering System/src/Main.java
@@ -0,0 +1,60 @@
+public class Main {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+
+    // Use cases for employee
+    public void createItem() {
+
+    }
+
+    public void updateItem() {
+
+    }
+
+    public void removeItem() {
+
+    }
+
+    // Order fullfilment
+    public Order[] retrieveOrders() {
+        return new Order[] { new Order(0, 0) }; // Temporary
+    }
+
+    public Order getOrderInformation() {
+        return new Order(0, 0); // Temporary
+    }
+
+    // Use cases for customer
+    public void startOrder() {
+
+    }
+
+    public void addItemToOrder() {
+
+    }
+
+    public void addModifier(IceCream iceCream) {
+
+    }
+
+    public void timeout() {
+
+    }
+
+    public void viewCart() {
+
+    }
+
+    public void addTip() {
+
+    }
+
+    public void checkOut() {
+
+    }
+
+    public void handlePayment() {
+
+    }
+}
