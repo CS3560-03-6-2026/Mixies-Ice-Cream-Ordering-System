a656f9b1ecba80a8341261a8f61cf0e7ed2be16a
Ahmad Sawalha <122925762+Abash05@users.noreply.github.com>
2026-04-23 16:45:20 -0700

    Add comments and clarify DBConnection class

--- Ice Cream Ordering System/src/DBConnection.java
+++ Ice Cream Ordering System/src/DBConnection.java
@@ -2,11 +2,14 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+// Utility Class to establish a connection to the database
 public class DBConnection {
+    // The database must be running on the same machine
     private static final String URL = "jdbc:mysql://localhost:3306/MixiesDB";
     private static final String USER = "root";
     private static final String PASSWORD = "cs3560mixies";
 
+    // Ensure MySQL driver is available
     static {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
@@ -15,7 +18,9 @@ public class DBConnection {
         }
     }
 
+    // Method to establish and return a database connection
+    // Throws SQLException for connection error
     public static Connection getConnection() throws SQLException {
-        return DriverManager.getConnection(URL, USER, PASSWORD);
+        return DriverManager.getConnection(URL, USER, PASSWORD); // Uses DriveManager
     }
 }
