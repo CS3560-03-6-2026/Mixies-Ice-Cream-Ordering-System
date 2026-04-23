da94a350e82a07e572cfbdb20d05a20f682932fd
Ahmad Sawalha <122925762+Abash05@users.noreply.github.com>
2026-04-22 21:06:30 -0700

    Update employee role in main application

--- Ice Cream Ordering System/src/Main.java
+++ Ice Cream Ordering System/src/Main.java
@@ -1,15 +1,10 @@
 import javax.swing.SwingUtilities;
 
 public class Main {
-    
     public static void main(String[] args) {
-
-
         SwingUtilities.invokeLater(() -> {
-            /// Employee loggedIn = new Employee(1, "Ava", employeeRoles.MANAGER);
-            new MixiesAppFrame().setVisible(true);
+            Employee loggedIn = new Employee(1, "Ava", employeeRoles.EMPLOYEE);
+            new MixiesAppFrame(loggedIn).setVisible(true);
         });
     }
-
-
 }
